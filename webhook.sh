e5ee0119fc3859d75c2916cf3469934bd3f0a64b
grimmygrom <153600500+grimmygrom@users.noreply.github.com>
2023-12-21 10:43:15 +0000

    Create webhook.sh

---
+++ webhook.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#put your discord webhook there so all of your friends in a server can get free nitro.
+#i do expect you to run this on a unix-like machine like macOS or linux
+webhook=""
+while true
+do
+	curl 'https://api.discord.gx.games/v1/direct-fulfillment' -H 'content-type: application/json' -d '{"partnerUserId":""}' -so /tmp/url.json;
+  url=$(jq -r '.token' /tmp/url.json | xargs -I {} echo https://discord.com/billing/partner-promotions/1180231712274387115/{})
+  curl -H "content-type: application/json" -d "{\"content\": \"${url}\"}" $webhook
+done
