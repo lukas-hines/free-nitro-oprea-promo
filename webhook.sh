bd98121940aedcbf5b9e55d4ebf328e0f45e86be
grimmygrom <153600500+grimmygrom@users.noreply.github.com>
2023-12-21 10:45:46 +0000

    Update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -4,7 +4,7 @@
 webhook=""
 while true
 do
-	curl 'https://api.discord.gx.games/v1/direct-fulfillment' -H 'content-type: application/json' -d '{"partnerUserId":""}' -so /tmp/url.json;
-  url=$(jq -r '.token' /tmp/url.json | xargs -I {} echo https://discord.com/billing/partner-promotions/1180231712274387115/{})
-  curl -H "content-type: application/json" -d "{\"content\": \"${url}\"}" $webhook
+	curl 'https://api.discord.gx.games/v1/direct-fulfillment' -H 'content-type: application/json' -d '{"partnerUserId":""}' -so /tmp/token.json;
+ 	url=$(jq -r '.token' /tmp/token.json | xargs -I {} echo https://discord.com/billing/partner-promotions/1180231712274387115/{})
+  	curl -H "content-type: application/json" -d "{\"content\": \"${url}\"}" $webhook
 done
